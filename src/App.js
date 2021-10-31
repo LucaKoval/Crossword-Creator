3326116344fd541a08b14eeccbe43cc34aa22cde
Luca Koval <lucakoval@me.com>
2021-10-30 22:35:31 -0400

    Initial setup, adding React

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+import './styles/global.css';
+import './styles/App.module.css';
+
+
+class App extends Component {
+  	constructor(props) {
+    	super(props);
+
+    	this.state = {
+    	};
+  	}
+
+  	render() {
+  		return(
+  			<div>
+  				Hi!
+  			</div>
+  		);
+  	}
+}
+
+export default App;

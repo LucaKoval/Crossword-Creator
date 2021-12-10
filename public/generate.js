eb8b2f905ca9288ebfcd76b11ade2ad22e27dd32
Luca Koval <lucakoval@me.com>
2021-12-10 16:24:24 -0500

    Adding WebWorker

---
+++ public/generate.js
@@ -0,0 +1,163 @@
+onmessage = function(e) {
+	// data via e.data
+	const {sortedWords, board, size, TimesUsed, Frequencies, frequenciesDenom, Dictionary} = e.data;
+
+	// Do DFS, fill the board
+  	let wordCounter = 0;
+  	let row = 0;
+  	let col = 0;
+  	let rowOrCol = 0;
+  	let foundWord = false;
+  	let counter = 0;
+  	// while (counter < 2000 && (row < size || col < size)) {
+  	while (row < size || col < size) {
+  		// Go back and replace the most-recently placed word
+  		if (rowOrCol > 0) {
+			rowOrCol--; // Get back
+
+			if (rowOrCol % 2 === 0) { // Clear row
+				// get # rows to clear
+				const amountToClear = Math.floor(Math.random() * row) + 1;
+
+				let wordsErased = [];
+
+				// clear rows w/ bound from columns
+				for (let i = 0; i < amountToClear; i++) {
+					let wordToErase = "";
+					for (let j = 0; j < size; j++) {
+						wordToErase += board[row-(i+1)][j];
+					}
+					wordsErased.push(wordToErase);
+
+					for (let j = col; j < size; j++) {
+						board[row-(i+1)][j] = "";
+					}
+				}
+
+				// reduce rows by proper amount
+				row -= amountToClear;
+
+				// get words erased, reduce times used by 1
+				wordsErased.forEach(word => {
+					TimesUsed[word]--;
+				});
+			} else { // Clear col
+				// get # cols to clear
+				const amountToClear = Math.floor(Math.random() * col) + 1;
+
+				let wordsErased = [];
+
+				// clear cols w/ bound from rows
+				for (let j = 0; j < amountToClear; j++) {
+					let wordToErase = "";
+					for (let i = 0; i < size; i++) {
+						wordToErase += board[i][col-(j+1)];
+					}
+					wordsErased.push(wordToErase);
+
+					for (let i = row; i < size; i++) {
+						board[i][col-(j+1)] = "";
+					}
+				}
+
+				// reduce cols by proper amount
+				col -= amountToClear;
+
+				// get words erased, reduce times used by 1
+				wordsErased.forEach(word => {
+					TimesUsed[word]--;
+				});
+			}
+		}
+
+		while (wordCounter < sortedWords.length && (row < size || col < size)) {
+			// Randomization
+			if (Math.random() < 0.35) {
+				// Random int example from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+				wordCounter = Math.floor(Math.random() * sortedWords.length)
+			}
+			const word = sortedWords[wordCounter];
+
+			if (word.length === size && TimesUsed[word] === 0) { // Fits in board (in simplified nxn case)
+				// Go through probabilistic acceptance
+				let frequenciesProduct = 1;
+				// let frequenciesDenom = this.state.frequenciesDenom;
+				word.split("").forEach(letter => {
+					const freq = Frequencies[letter] === undefined ? 0 : Frequencies[letter]/frequenciesDenom;
+					frequenciesProduct *= freq;
+				});
+
+				// The higher the frequency, the lower the chance of rejecting and the higher the chance of accepting
+				if (Math.random() < frequenciesProduct*100) { // Accept
+					// Check that the word works with the rest of the board
+	  				let fits = true;
+	  				for (let i = 0; i < word.length; i++) {
+	  					let boardLetter;
+	  					if (rowOrCol % 2 === 0) { // Trying a row
+	  						boardLetter = board[row][i];
+	  					} else { // Trying a col
+	  						boardLetter = board[i][col];
+	  					}
+
+	  					if (boardLetter != "" && boardLetter != word[i]) fits = false;
+	  				}
+
+	  				if (fits) { // If the word was placed/is able to be placed
+	  					for (let i = 0; i < word.length; i++) {
+		  					if (rowOrCol % 2 === 0) { // Trying a row
+		  						board[row][i] = word[i];
+		  					} else { // Trying a col
+		  						board[i][col] = word[i];
+		  					}
+		  				}
+		  				foundWord = true;
+		  				TimesUsed[word]++;
+	  				}
+				}
+			}
+
+			if (foundWord) {
+	  			if (rowOrCol % 2 === 0) { // Filled in a row
+	  				if (col < size) { // We haven't filled all columns, so switch rowOrCol
+	  					rowOrCol++;
+	  				}
+	  				row++;
+	  			} else { // Filled in a col
+	  				if (row < size) { // We haven't filled all rows, so switch rowOrCol
+	  					rowOrCol++;
+	  				}
+	  				col++;
+	  			}
+	  			foundWord = false;
+	  		}
+
+			wordCounter++;
+		}
+		wordCounter = 0;
+		counter++;
+	}
+
+	// Get clues, aka definitions
+	let clues = [];
+	let rowDefinitions = [];
+	let colDefinitions = [];
+
+	// Get words
+	for (let i = 0; i < size; i++) {
+		let rowWord = "";
+		let colWord = "";
+
+		for (let j = 0; j < size; j++) {
+			rowWord += board[i][j];
+			colWord += board[j][i];
+		}
+		const rowDefinition = Dictionary[rowWord];
+		rowDefinitions.push(rowDefinition);
+		const colDefinition = Dictionary[colWord];
+		colDefinitions.push(colDefinition);
+	}
+	clues = clues.concat(colDefinitions);
+	clues = clues.concat(rowDefinitions);
+
+	postMessage({ data: board, clues: clues });
+}
--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Crossword from './components/Crossword';
 import ProblemInput from './components/ProblemInput';
 import SortedWords from './data/sortedWords';
 import TimesUsed from './data/timesUsed';
+// import Worker from "./workers/generate.js";
 
 // Dictionary is from https://github.com/matthewreagan/WebstersEnglishDictionary
 import Dictionary from './data/dictionary_compact';
@@ -24,6 +25,7 @@ class App extends Component {
     		data: [],
     		sortedWords: [],
     		frequenciesDenom: 40000,
+    		worker: undefined,
     	};
   	}
 
@@ -32,10 +34,10 @@ class App extends Component {
   		this.setState({ sortedWords: sortedWords });
 
 		this.setState({ sortedWords: SortedWords["text"] });
-  		this.clearBoard();
+  		this.setState({ data: this.generateClearBoard() });
   	}
 
-  	clearBoard() {
+  	generateClearBoard() {
   		let board = [];
   		for (let i = 0; i < this.state.size; i++) {
   			let row = [];
@@ -44,172 +46,205 @@ class App extends Component {
   			}
   			board.push(row);
   		}
-  		this.setState({ data: board });
+  		return board;
   	}
 
   	generateData = () => {
-  		const sortedWords = this.state.sortedWords;
-  		this.clearBoard();
-  		const board = this.state.data;
+  		// const sortedWords = this.state.sortedWords;
+  		const clearBoard = this.generateClearBoard()
+  		// const board = this.state.data;
 
-  		// Do DFS, fill the board
-  		let wordCounter = 0;
-  		let row = 0;
-  		let col = 0;
-  		let rowOrCol = 0;
-  		let foundWord = false;
-  		let counter = 0;
-  		// while (counter < 2000 && (row < this.state.size || col < this.state.size)) {
-  		while (row < this.state.size || col < this.state.size) {
-  			// Go back and replace the most-recently placed word
-  			if (rowOrCol > 0) {
-	  			rowOrCol--; // Get back
 
-	  			if (rowOrCol % 2 === 0) { // Clear row
-	  				// get # rows to clear
-	  				const amountToClear = Math.floor(Math.random() * row) + 1;
-
-	  				let wordsErased = [];
-
-	  				// clear rows w/ bound from columns
-	  				for (let i = 0; i < amountToClear; i++) {
-	  					let wordToErase = "";
-	  					for (let j = 0; j < this.state.size; j++) {
-	  						wordToErase += board[row-(i+1)][j];
-	  					}
-	  					wordsErased.push(wordToErase);
-
-	  					for (let j = col; j < this.state.size; j++) {
-	  						board[row-(i+1)][j] = "";
-	  					}
-	  				}
-
-	  				// reduce rows by proper amount
-	  				row -= amountToClear;
-
-	  				// get words erased, reduce times used by 1
-	  				wordsErased.forEach(word => {
-	  					TimesUsed[word]--;
-	  				});
-	  			} else { // Clear col
-	  				// get # cols to clear
-	  				const amountToClear = Math.floor(Math.random() * col) + 1;
-
-	  				let wordsErased = [];
-
-	  				// clear cols w/ bound from rows
-	  				for (let j = 0; j < amountToClear; j++) {
-	  					let wordToErase = "";
-	  					for (let i = 0; i < this.state.size; i++) {
-	  						wordToErase += board[i][col-(j+1)];
-	  					}
-	  					wordsErased.push(wordToErase);
-
-	  					for (let i = row; i < this.state.size; i++) {
-	  						board[i][col-(j+1)] = "";
-	  					}
-	  				}
-
-	  				// reduce cols by proper amount
-	  				col -= amountToClear;
-
-	  				// get words erased, reduce times used by 1
-	  				wordsErased.forEach(word => {
-	  					TimesUsed[word]--;
-	  				});
-	  			}
-	  		}
-
-	  		while (wordCounter < sortedWords.length && (row < this.state.size || col < this.state.size)) {
-	  			// Randomization
-	  			if (Math.random() < 0.35) {
-	  				// Random int example from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-	  				wordCounter = Math.floor(Math.random() * sortedWords.length)
-	  			}
-	  			const word = sortedWords[wordCounter];
-
-	  			if (word.length === this.state.size && TimesUsed[word] === 0) { // Fits in board (in simplified nxn case)
-	  				// Go through probabilistic acceptance
-	  				let frequenciesProduct = 1;
-	  				let frequenciesDenom = this.state.frequenciesDenom;
-	  				word.split("").forEach(letter => {
-	  					const freq = Frequencies[letter] === undefined ? 0 : Frequencies[letter]/frequenciesDenom;
-	  					frequenciesProduct *= freq;
-	  				});
-
-	  				// The higher the frequency, the lower the chance of rejecting and the higher the chance of accepting
-	  				if (Math.random() < frequenciesProduct*100) { // Accept
-	  					// Check that the word works with the rest of the board
-		  				let fits = true;
-		  				for (let i = 0; i < word.length; i++) {
-		  					let boardLetter;
-		  					if (rowOrCol % 2 === 0) { // Trying a row
-		  						boardLetter = board[row][i];
-		  					} else { // Trying a col
-		  						boardLetter = board[i][col];
-		  					}
-
-		  					if (boardLetter != "" && boardLetter != word[i]) fits = false;
-		  				}
-
-		  				if (fits) { // If the word was placed/is able to be placed
-		  					for (let i = 0; i < word.length; i++) {
-			  					if (rowOrCol % 2 === 0) { // Trying a row
-			  						board[row][i] = word[i];
-			  					} else { // Trying a col
-			  						board[i][col] = word[i];
-			  					}
-			  				}
-			  				foundWord = true;
-			  				TimesUsed[word]++;
-		  				}
-	  				}
-	  			}
-
-	  			if (foundWord) {
-		  			if (rowOrCol % 2 === 0) { // Filled in a row
-		  				if (col < this.state.size) { // We haven't filled all columns, so switch rowOrCol
-		  					rowOrCol++;
-		  				}
-		  				row++;
-		  			} else { // Filled in a col
-		  				if (row < this.state.size) { // We haven't filled all rows, so switch rowOrCol
-		  					rowOrCol++;
-		  				}
-		  				col++;
-		  			}
-		  			foundWord = false;
-		  		}
-
-	  			wordCounter++;
-	  		}
-	  		wordCounter = 0;
-	  		counter++;
+  		let oldWorker = this.state.worker;
+  		if (oldWorker !== undefined) {
+	  		oldWorker.terminate();
+	  		oldWorker = undefined;
+	  		this.setState({ worker: oldWorker });
 	  	}
 
-	  	// Get clues, aka definitions
-	  	let clues = [];
-	  	let rowDefinitions = [];
-	  	let colDefinitions = [];
+  		let worker = new window.Worker("./generate.js");
+  		this.setState({ worker: worker });
+  		worker.postMessage({ 
+  			sortedWords: this.state.sortedWords,
+  			board: clearBoard,
+  			size: this.state.size,
+  			TimesUsed: TimesUsed,
+  			Frequencies: Frequencies,
+  			frequenciesDenom: this.state.frequenciesDenom,
+  			Dictionary: Dictionary,
+  		});
 
-	  	// Get words
-	  	for (let i = 0; i < this.state.size; i++) {
-	  		let rowWord = "";
-	  		let colWord = "";
+  		let thisComponent = this;
+  		worker.onmessage = function(e) {
+  			thisComponent.setState({ data: e.data.data, clues: e.data.clues });
 
-	  		for (let j = 0; j < this.state.size; j++) {
-	  			rowWord += board[i][j];
-	  			colWord += board[j][i];
-	  		}
-	  		const rowDefinition = Dictionary[rowWord];
-	  		rowDefinitions.push(rowDefinition);
-	  		const colDefinition = Dictionary[colWord];
-	  		colDefinitions.push(colDefinition);
-	  	}
-	  	clues = clues.concat(colDefinitions);
-	  	clues = clues.concat(rowDefinitions);
+  			worker.terminate();
+  			worker = undefined;
+  			thisComponent.setState({ worker: worker });
+  		}
 
-  		this.setState({ data: board, clues: clues });
+
+
+
+
+  		// // Do DFS, fill the board
+  		// let wordCounter = 0;
+  		// let row = 0;
+  		// let col = 0;
+  		// let rowOrCol = 0;
+  		// let foundWord = false;
+  		// let counter = 0;
+  		// // while (counter < 2000 && (row < this.state.size || col < this.state.size)) {
+  		// while (row < this.state.size || col < this.state.size) {
+  		// 	// Go back and replace the most-recently placed word
+  		// 	if (rowOrCol > 0) {
+	  	// 		rowOrCol--; // Get back
+
+	  	// 		if (rowOrCol % 2 === 0) { // Clear row
+	  	// 			// get # rows to clear
+	  	// 			const amountToClear = Math.floor(Math.random() * row) + 1;
+
+	  	// 			let wordsErased = [];
+
+	  	// 			// clear rows w/ bound from columns
+	  	// 			for (let i = 0; i < amountToClear; i++) {
+	  	// 				let wordToErase = "";
+	  	// 				for (let j = 0; j < this.state.size; j++) {
+	  	// 					wordToErase += board[row-(i+1)][j];
+	  	// 				}
+	  	// 				wordsErased.push(wordToErase);
+
+	  	// 				for (let j = col; j < this.state.size; j++) {
+	  	// 					board[row-(i+1)][j] = "";
+	  	// 				}
+	  	// 			}
+
+	  	// 			// reduce rows by proper amount
+	  	// 			row -= amountToClear;
+
+	  	// 			// get words erased, reduce times used by 1
+	  	// 			wordsErased.forEach(word => {
+	  	// 				TimesUsed[word]--;
+	  	// 			});
+	  	// 		} else { // Clear col
+	  	// 			// get # cols to clear
+	  	// 			const amountToClear = Math.floor(Math.random() * col) + 1;
+
+	  	// 			let wordsErased = [];
+
+	  	// 			// clear cols w/ bound from rows
+	  	// 			for (let j = 0; j < amountToClear; j++) {
+	  	// 				let wordToErase = "";
+	  	// 				for (let i = 0; i < this.state.size; i++) {
+	  	// 					wordToErase += board[i][col-(j+1)];
+	  	// 				}
+	  	// 				wordsErased.push(wordToErase);
+
+	  	// 				for (let i = row; i < this.state.size; i++) {
+	  	// 					board[i][col-(j+1)] = "";
+	  	// 				}
+	  	// 			}
+
+	  	// 			// reduce cols by proper amount
+	  	// 			col -= amountToClear;
+
+	  	// 			// get words erased, reduce times used by 1
+	  	// 			wordsErased.forEach(word => {
+	  	// 				TimesUsed[word]--;
+	  	// 			});
+	  	// 		}
+	  	// 	}
+
+	  	// 	while (wordCounter < sortedWords.length && (row < this.state.size || col < this.state.size)) {
+	  	// 		// Randomization
+	  	// 		if (Math.random() < 0.35) {
+	  	// 			// Random int example from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+	  	// 			wordCounter = Math.floor(Math.random() * sortedWords.length)
+	  	// 		}
+	  	// 		const word = sortedWords[wordCounter];
+
+	  	// 		if (word.length === this.state.size && TimesUsed[word] === 0) { // Fits in board (in simplified nxn case)
+	  	// 			// Go through probabilistic acceptance
+	  	// 			let frequenciesProduct = 1;
+	  	// 			let frequenciesDenom = this.state.frequenciesDenom;
+	  	// 			word.split("").forEach(letter => {
+	  	// 				const freq = Frequencies[letter] === undefined ? 0 : Frequencies[letter]/frequenciesDenom;
+	  	// 				frequenciesProduct *= freq;
+	  	// 			});
+
+	  	// 			// The higher the frequency, the lower the chance of rejecting and the higher the chance of accepting
+	  	// 			if (Math.random() < frequenciesProduct*100) { // Accept
+	  	// 				// Check that the word works with the rest of the board
+		  // 				let fits = true;
+		  // 				for (let i = 0; i < word.length; i++) {
+		  // 					let boardLetter;
+		  // 					if (rowOrCol % 2 === 0) { // Trying a row
+		  // 						boardLetter = board[row][i];
+		  // 					} else { // Trying a col
+		  // 						boardLetter = board[i][col];
+		  // 					}
+
+		  // 					if (boardLetter != "" && boardLetter != word[i]) fits = false;
+		  // 				}
+
+		  // 				if (fits) { // If the word was placed/is able to be placed
+		  // 					for (let i = 0; i < word.length; i++) {
+			 //  					if (rowOrCol % 2 === 0) { // Trying a row
+			 //  						board[row][i] = word[i];
+			 //  					} else { // Trying a col
+			 //  						board[i][col] = word[i];
+			 //  					}
+			 //  				}
+			 //  				foundWord = true;
+			 //  				TimesUsed[word]++;
+		  // 				}
+	  	// 			}
+	  	// 		}
+
+	  	// 		if (foundWord) {
+		  // 			if (rowOrCol % 2 === 0) { // Filled in a row
+		  // 				if (col < this.state.size) { // We haven't filled all columns, so switch rowOrCol
+		  // 					rowOrCol++;
+		  // 				}
+		  // 				row++;
+		  // 			} else { // Filled in a col
+		  // 				if (row < this.state.size) { // We haven't filled all rows, so switch rowOrCol
+		  // 					rowOrCol++;
+		  // 				}
+		  // 				col++;
+		  // 			}
+		  // 			foundWord = false;
+		  // 		}
+
+	  	// 		wordCounter++;
+	  	// 	}
+	  	// 	wordCounter = 0;
+	  	// 	counter++;
+	  	// }
+
+	  	// // Get clues, aka definitions
+	  	// let clues = [];
+	  	// let rowDefinitions = [];
+	  	// let colDefinitions = [];
+
+	  	// // Get words
+	  	// for (let i = 0; i < this.state.size; i++) {
+	  	// 	let rowWord = "";
+	  	// 	let colWord = "";
+
+	  	// 	for (let j = 0; j < this.state.size; j++) {
+	  	// 		rowWord += board[i][j];
+	  	// 		colWord += board[j][i];
+	  	// 	}
+	  	// 	const rowDefinition = Dictionary[rowWord];
+	  	// 	rowDefinitions.push(rowDefinition);
+	  	// 	const colDefinition = Dictionary[colWord];
+	  	// 	colDefinitions.push(colDefinition);
+	  	// }
+	  	// clues = clues.concat(colDefinitions);
+	  	// clues = clues.concat(rowDefinitions);
+
+  		// this.setState({ data: board, clues: clues });
   	}
 
   	render() {

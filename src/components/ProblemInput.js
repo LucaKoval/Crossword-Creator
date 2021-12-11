53c31ef2f601837d415e1d4a184ec8ae65a0fee1
Isabel Selin <isabelselin@gmail.com>
2021-12-11 18:41:57 -0500

    temp commit

--- src/components/ProblemInput.js
+++ src/components/ProblemInput.js
@@ -12,19 +12,43 @@ class ProblemInput extends Component {
     };
   }
 
+  reader = null;
+
   onFileChange = event => {
     this.setState({ selectedFile: event.target.files[0] });
+    this.reader = new FileReader(event.target.files[0]);
+    console.log(event.target.files[0])
+  };
+
+  problemInfo = data => {
+    console.log("recalculating problemInfo");
+    // if (data){
+    //   let parsedData = JSON.parse(data);
+    //   console.log(parsedData);
+    //   // return(
+    //   //   <div>
+    //   //     {
+    //   //       "Board width: " + data.board.width +
+    //   //       "\nBoard height: " + data.board.height
+    //   //     }
+    //   //   </div>
+    //   // );
+    //   return;
+    // }
+    // else return;
   };
 
   render() {
     return(
       <div className="buttonContainer">
+
         <input
             type="file"
             className="button"
             onChange={this.onFileChange}
         />
         <div className="buttonShadow" />
+        {this.problemInfo(this.state.selectedFile)}
       </div>
 
     );

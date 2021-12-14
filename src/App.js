191e6bd5b7ff7d0df1f927181a63fc0831f106f1
Isabel Selin <isabelselin@gmail.com>
2021-12-13 23:14:21 -0500

    Added generation of 10 cases simultaneously for testing

--- src/App.js
+++ src/App.js
@@ -24,9 +24,14 @@ class App extends Component {
     		data: [],
     		sortedWords: [],
     		frequenciesDenom: 40000,
+
     		worker: undefined,
 				inProgress: false,
 				generationTime: 0,
+				
+				manyWorkers: [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+				manyInProgress: 0,
+				manyTime: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     	};
   	}
 
@@ -75,6 +80,7 @@ class App extends Component {
   			Dictionary: Dictionary,
   		});
 
+			//deletes the thread & returns the time it took to terminate
   		let thisComponent = this;
   		worker.onmessage = function(e) {
 				const endTime = Math.trunc(performance.now());
@@ -93,6 +99,66 @@ class App extends Component {
   		}
   	}
 
+		generateManyData = () => {
+			const startTime = Math.trunc(performance.now());
+			this.setState({ manyProgress: 10 });
+			const clearBoard = this.generateClearBoard()
+			let newWorkers = [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined];
+
+			//reset all manyWorkers
+			for (let i = 0; i < 10; i++) {
+				let oldWorker = this.state.manyWorkers[i];
+				if (oldWorker !== undefined) {
+					oldWorker.terminate();
+				}
+				this.setState({ manyWorkers: newWorkers });
+			}
+
+			
+			for  (let i = 0; i < 10; i++) {
+				let worker = new window.Worker("./generate.js");
+				newWorkers[i] = worker;
+				worker.postMessage({ 
+					sortedWords: this.state.sortedWords,
+					board: clearBoard,
+					size: this.state.size,
+					TimesUsed: TimesUsed,
+					Frequencies: Frequencies,
+					frequenciesDenom: this.state.frequenciesDenom,
+					Dictionary: Dictionary,
+				});
+
+				//deletes the thread & returns the time it took to terminate
+				let thisComponent = this;
+				worker.onmessage = function(e) {
+					worker.terminate();
+					let tempWorkers = thisComponent.state.manyWorkers.slice(0);
+					tempWorkers[i] = undefined;
+
+					let tempTime = thisComponent.state.manyTime.slice(0);
+					const endTime = Math.trunc(performance.now());
+					tempTime[i] = (endTime-startTime)/1000;;
+					console.log(i);
+
+					thisComponent.setState((prevState) => ({
+						manyInProgress: prevState.manyInProgress - 1,
+						manyWorkers: tempWorkers,
+						manyTime: tempTime,
+					}));
+				}
+			}
+			this.setState({ manyWorkers: newWorkers });
+  	}
+
+		printManyTime = () => {
+			let outputStr = "["
+			for (let i = 0; i < 10; i++){
+				outputStr = outputStr + this.state.manyTime[i] + "s, ";
+			}
+			outputStr = outputStr.slice(0, outputStr.length - 2) + "]";
+			return outputStr;
+		}
+
   	render() {
   		return(
   			<div className={styles.container}>
@@ -109,19 +175,32 @@ class App extends Component {
 	  					/>
 	  				</div>
 	  				<div className="buttonContainer">
-	            		<input
-	              			type="Submit"
-	              			className={this.state.inProgress?"inactiveButton":"button"}
-	              			value={this.state.inProgress?"In progress":"Generate new"}
-	              			onClick={this.generateData}
-	            		/>
-	            		<div className={this.state.inProgress?"inactiveShadow":"buttonShadow"} />
-	          		</div>
-	        	</div>
+							<input
+								type="Submit"
+								className={this.state.inProgress?"inactiveButton":"button"}
+								value={this.state.inProgress?"In progress":"Generate new"}
+								onClick={this.generateData}
+							/>
+							<div className={this.state.inProgress?"inactiveShadow":"buttonShadow"} />
+	          </div>
+						<div className="buttonContainer">
+							<input
+								type="Submit"
+								className={(this.state.manyProgress > 0)?"inactiveButton":"button"}
+								value={(this.state.manyProgress > 0)
+									? this.state.manyProgress + " in progress"
+									:"Generate new x10"}
+								onClick={this.generateManyData}
+							/>
+							<div className={(this.state.manyProgress > 0)?"inactiveShadow":"buttonShadow"} />
+	          </div>
 						<p>
-
-							Time to generate: {this.state.generationTime} seconds
+							Time to generate 1: {this.state.generationTime} seconds
 						</p>
+						<p>
+							Time to generate 10: {this.printManyTime()}
+						</p>
+	        </div>
   			</div>
   		);
   	}
